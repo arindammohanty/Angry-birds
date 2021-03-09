35c461c191a9aeba0b29dbe8cab9802f54f81f61
ArindamMohanty <69613665+arindammohanty@users.noreply.github.com>
2021-03-09 12:44:50 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,7 +9,7 @@ var backgroundImg,platform;
 var bird, slingshot;
 
 var gameState = "onSling";
-var bg = "sprites/bg1.png";
+var bg = "bg1.png";
 var score = 0;
 
 function preload() {
@@ -109,10 +109,10 @@ async function getBackgroundImg(){
     var hour = datetime.slice(11,13);
     
     if(hour>=0600 && hour<=1900){
-        bg = "sprites/bg1.png";
+        bg = "bg1.png";
     }
     else{
-        bg = "sprites/bg2.jpg";
+        bg = "bg2.jpg";
     }
 
     backgroundImg = loadImage(bg);
